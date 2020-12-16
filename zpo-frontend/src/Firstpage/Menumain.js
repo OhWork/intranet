825e064916be36a23c9aac3ca54127a8909988a4
Firore <takbasic722@gmail.com>
2020-12-16 22:35:15 +0700

    fontend

---
+++ zpo-frontend/src/Firstpage/Menumain.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+class Menumain extends Component {
+    render(){
+        return
+    }
+}
+
+export default Menumain;
